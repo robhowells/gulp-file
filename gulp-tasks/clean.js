ec157515d4442a431f9a9358e1be755d3859d210
Rob Howells <Rob.Howells@FelineSoft.local>
2017-07-12 17:20:32 +0100

    updated gitignore settings to fix issue with dist directory and remove need for gulp init task

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -6,6 +6,6 @@
 
 module.exports = function (base, paths, config, gulp, plugins, del) {
 	return function () {
-		del.sync(base.dist);
+		del.sync([base.dist, '!' + base.dist + '.gitkeep']);
 	};
 };
